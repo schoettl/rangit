0c08545d95f6a48a075c1cb3aacb9ab521a1b004
Jakob Schöttl <jschoett@gmail.com>
2015-09-30 23:09:44 -0700

    Added test cases

--- haskell/test-suite/Rangit/DriveSpec.hs
+++ haskell/test-suite/Rangit/DriveSpec.hs
@@ -15,43 +15,61 @@ spec = do
         context "driving a car to the right (positive distance)" $ do
             let movedTrain = drive train 1 0
                 [movedCar] = movedTrain
-                Position x y = partPosition movedCar
             it "calculates new position correctly" $ do
+                let Position x y = partPosition movedCar
                 x `shouldSatisfy` \x -> (x > (1-stepLength)) && x < (1+stepLength)
             it "calculates new angle" $ do
                 partAngle movedCar `shouldBe` 0
         context "driving a car to the left (negative distance)" $ do
             let movedTrain = drive train (-1) 0
                 [movedCar] = movedTrain
-                Position x y = partPosition movedCar
             it "calculates new position correctly" $ do
+                let Position x y = partPosition movedCar
                 x `shouldSatisfy` \x -> (x > (-1-stepLength)) && x < (-1+stepLength)
                 y `shouldBe` 0
-            it "calculates new angle" $ do
+            it "calculates new angle correctly" $ do
                 partAngle movedCar `shouldBe` 0
         context "driving car with trailer" $ do
             let trailer = Part origin 0 undefined 1
                 trainWithTrailer = fixInitialPositions $ trailer:train
                 movedTrain = drive trainWithTrailer (-1) 0
                 movedTrailer:_ = movedTrain
-                Position x y = partPosition movedTrailer
             it "calculates new trailer position" $ do
+                let Position x y = partPosition movedTrailer
                 x `shouldSatisfy` \x -> (x > (-2-stepLength)) && x < (-2+stepLength)
                 y `shouldBe` 0
-            it "calculates new angle" $ do
+            it "calculates new angle correctly" $ do
                 partAngle movedTrailer `shouldBe` 0
-
+        context "bringing car from <90° to >90°" $ do
+            let train = [Part origin (pi/2-0.1) 0 1]
+                movedTrain = drive train 1 (pi/4)
+                [movedCar] = movedTrain
+            it "calculates new position plausible" $ do
+                let Position x y = partPosition movedCar
+                x `shouldSatisfy` (<0)
+                y `shouldSatisfy` (>0)
+            it "calculates new angle correctly" $ do
+                partAngle movedCar `shouldSatisfy` (>pi/2)
 
     describe "movePart" $ do
         context "move one part" $ do
             let powerCar = Part origin 0 0 1
                 targetPosition = Position 1 1
                 newAngle = atan $ 1 / (1 + 1) -- 1 from dimensions of power car
-                ([pc], lh) = movePart powerCar ([], targetPosition)
+                ([pc], _) = movePart powerCar ([], targetPosition)
             it "updates the position" $ do
                 partPosition pc `shouldBe` targetPosition
             it "updates the angle" $ do
                 partAngle pc @=~? newAngle
+        context "move part from <90° to >90°" $ do
+            let part = Part origin (pi/2) 0 1
+                targetPosition = Position (-1) 1
+                ([movedPart], _) = movePart part ([], targetPosition)
+            it "updates position right" $ do
+                partPosition movedPart `shouldBe` targetPosition
+            it "updates angle right" $ do
+                partAngle movedPart `shouldSatisfy` (>pi/2)
+                partAngle movedPart `shouldSatisfy` (<pi)
 
     describe "calculateAngleByArcTan" $ do
         --it "works for an angle less than 45°" $ do
@@ -64,3 +82,11 @@ spec = do
             calculateAngleByArcTan (-1) 0 `shouldBe` pi
         it "works for -90°" $ do
             calculateAngleByArcTan 0 (-1) `shouldBe` (-pi/2)
+        it "works for x and y equals 0" $ do
+            calculateAngleByArcTan 0 0 `shouldBe` 0
+        it "angles below the horizontal line are negative" $ property $
+            \ x -> calculateAngleByArcTan x (-1) < 0
+        it "angles above the horizontal line are positive" $ property $
+            \ x -> calculateAngleByArcTan x 1 > 0
+        it "angles mirrowed at the horizontal line differ olny in sign" $ property $
+            \ x -> calculateAngleByArcTan x 1 == -calculateAngleByArcTan x (-1)
