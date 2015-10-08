e9f9284373b79759fc2c904ca7839eea4dccf79f
Jakob Schöttl <jschoett@gmail.com>
2015-10-08 12:57:30 -0700

    Added executable backup AI

---
+++ haskell/src/backupai/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import System.IO
+import Data.Angle
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Aeson
+import Rangit.Train
+import Rangit.Drive
+import System.Console.Docopt
+
+-- | Output drive commands for a given train to drive a given route.
+main :: IO ()
+main = do
+    return ()
