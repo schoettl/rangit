b777c0c493ef06cb203384e75c400b7dde514bdf
Jakob Schöttl <jschoett@gmail.com>
2015-10-29 13:19:31 -0700

    generatepaths parameterizable

--- haskell/test-backupai/generate_paths.sh
+++ haskell/test-backupai/generate_paths.sh
@@ -2,7 +2,9 @@
 
 # run it from it's directory!
 
-mkdir -p paths/
-rm paths/*
+usage="$0 <xstep>"
 
-../dist/build/generatepaths/generatepaths paths/
+mkdir -p paths/
+rm -f paths/*
+
+../dist/build/generatepaths/generatepaths -x "$1" paths/ &>/dev/null || echo "usage: $usage" >&2
