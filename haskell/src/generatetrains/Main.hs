52e0ffb226de4226e3eff45691a3d061fbb15671
Jakob Schöttl <jschoett@gmail.com>
2015-11-05 11:05:31 -0800

    Added power car with rear steer

--- haskell/src/generatetrains/Main.hs
+++ haskell/src/generatetrains/Main.hs
@@ -20,8 +20,11 @@ powerCar = Part origin 0 0 3
 trailer1 = Part origin 0 0 1
 trailer2 = Part origin 0 0 3
 
+powerCarRearSteer = Part origin 0 (-3) 3
+
 powerCarTrain = [powerCar]
 twoAxesTrailer = [trailer2, trailer1]
+powerCarRearSteerTrain = [powerCarRearSteer]
 
 -- | Trains to be saved. First value in the tuple is the one-word description.
 -- Please one word only because it is also used as a part of the result
@@ -30,6 +33,7 @@ trains =
     [ ("car"              , powerCarTrain)
     , ("trailer_car"      , trailer2 : powerCarTrain)
     , ("2axestrailer_car" , twoAxesTrailer ++ powerCarTrain)
+    , ("carrearsteer"     , powerCarRearSteerTrain)
     ]
 
 -- main
